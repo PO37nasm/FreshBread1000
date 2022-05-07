b4ff3a803baa7111f96b94c6a8416dc5e7ff5b7b
PO37nasm <80705111+PO37nasm@users.noreply.github.com>
2022-05-07 17:48:56 +0100

    Duck handles like a boat.

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     [SerializeField]
-    private CharacterController controller;
-    [SerializeField]
     private Rigidbody body;
     [SerializeField]
     private float speed = 6f;
+    [SerializeField]
+    private float rotationTorque = 0.1f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        //Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
+        if (vertical >= 0.1f)
         {
-            body.AddForce(direction * speed * Time.deltaTime, ForceMode.VelocityChange);
+            //float targetAngle = Mathf.Atan2(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.z) * Mathf.Rad2Deg;
+            //body.AddTorque(0f, targetAngle * rotationTorque, 0f);
+
+            Vector3 facing = transform.forward;
+            Debug.Log(facing);
+            body.AddForce( facing * speed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (horizontal >= 0.1f || horizontal <= -0.1f)
+        {
+            body.AddTorque(0f, horizontal, 0f);
         }
     }
 }
