58f3bf0da563aa1614fdb469b309d3f2265fcabf
MattWatson97 <104858585+MattWatson97@users.noreply.github.com>
2022-05-09 22:41:36 +0100

    Test scene 4 update
    Added all sounds from scene 3 into scene 4, among adding other subtle sounds and experimenting with FMOD based scripting.

--- Assets/Audio/F_BoatMotor.cs
+++ Assets/Audio/F_BoatMotor.cs
@@ -6,18 +6,23 @@ public class F_BoatMotor : MonoBehaviour
 {
 
     FMOD.Studio.EventInstance BoatMotor;
+    FMOD.Studio.EventInstance BoatRevs;
 
     // Start is called before the first frame update
     void Start()
     {
         BoatMotor = FMODUnity.RuntimeManager.CreateInstance("event:/Boat/BoatMotor");
         BoatMotor.start();
+        BoatRevs = FMODUnity.RuntimeManager.CreateInstance("event:/Boat/BoatRevs");
+        BoatRevs.start();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(BoatMotor, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(BoatRevs, GetComponent<Transform>(), GetComponent<Rigidbody>());
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
@@ -25,12 +30,12 @@ public class F_BoatMotor : MonoBehaviour
         if (vertical >= 0.1f || horizontal >= 0.1f || horizontal <= -0.1f)
         {
             BoatMotor.setParameterByName("BoatMotion", 0f, false);
-            Debug.Log("Boat moving");
+            BoatRevs.setParameterByName("BoatAccelDecel", 0f);
         }
         else
         {
             BoatMotor.setParameterByName("BoatMotion", 10f, false);
-            Debug.Log("Boat idle");
+            BoatRevs.setParameterByName("BoatAccelDecel", 1f);
         }
     }
 }
